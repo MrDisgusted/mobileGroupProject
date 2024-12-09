d7e98273c181f116edc71e30fe4ce809da177dd5
MrDisgusted <202101534@student.polytechnic.bh>
2024-12-09 14:22:46 +0300

    main commit

--- Lavender/Lavender/ViewController.swift
+++ Lavender/Lavender/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RecommendedTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        RecommendedTable.delegate = self
+        RecommendedTable.dataSource = self
     }
-
-
 }
+
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: <#T##IndexPath#>)
+        
+        return cell
+    }
+}
+
 
