e890f23370985b942218c71e94428082f90abab3
MrDisgusted <202101534@student.polytechnic.bh>
2024-12-10 16:22:04 +0300

    added product page

---
+++ Lavender/Lavender/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  Lavender
+//
+//  Created by BP-36-201-05 on 10/12/2024.
+//
+
+import Foundation
---
+++ Lavender/Lavender/ProductTableView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductTableView.swift
+//  Lavender
+//
+//  Created by BP-36-201-05 on 10/12/2024.
+//
+
+import UIKit
+
+class ProductTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
